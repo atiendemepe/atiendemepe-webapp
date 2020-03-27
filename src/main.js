f6994914b0b899a92ec1a24d61f657e93da92200
jaxkodex <jorge12745@hotmail.com>
2020-03-26 21:07:58 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+
+import firebase from 'firebase'
+
+import './assets/css/theme.scss'
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyDSfszcaozKu4VKLrpPTfdP4sL9OuY3yEY",
+  authDomain: "atiendeme-pe-1561952307583.firebaseapp.com",
+  databaseURL: "https://atiendeme-pe-1561952307583.firebaseio.com",
+  projectId: "atiendeme-pe-1561952307583",
+  storageBucket: "atiendeme-pe-1561952307583.appspot.com",
+  messagingSenderId: "978312703758",
+  appId: "1:978312703758:web:800c644627d1413a5b715f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1 class="title">Ingresar ahora</h1>
+    <p>Hola</p>
+    <button class="button is-primary is-fullwidth">Ingresar</button>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from 'firebase'
+import * as firebaseui from 'firebaseui'
+
+const uiConfig = {
+  signInSuccessUrl: 'http://localhost:8081/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  tosUrl: 'http://localhost:8081/tos',
+  privacyPolicyUrl: 'http://localhost:8081/privacy',
+};
+
+export default {
+  name: 'Login',
+  mounted() {
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+}
+</script>
